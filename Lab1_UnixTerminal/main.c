34dc93970d3487693d5d5ea2ea6c079d81b94d55
Ritvik Rao <rrao@login-students.ccs.neu.edu>
2019-09-09 12:52:46 -0400

    Lab commit before adding screenshot

--- Lab1_UnixTerminal/main.c
+++ Lab1_UnixTerminal/main.c
@@ -1,1 +1,14 @@
 // Add your program here!
+#include <stdio.h>
+
+double power(double base, double n){
+	if(n==0) return 1;
+	return base*power(base, n-1);
+}
+
+int main(){
+	for(int i=1; i<=10; i++){
+		printf("2^%d=%f\n", i, power(2, i));
+	}
+	return 0;
+}
