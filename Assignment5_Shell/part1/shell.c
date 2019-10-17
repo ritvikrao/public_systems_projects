bc195f3ae5c89ad9a2caac1bfbb8d4b056250456
ritvikrao <ritvikvinayak@gmail.com>
2019-10-17 19:27:44 -0400

    added personal function

--- Assignment5_Shell/part1/shell.c
+++ Assignment5_Shell/part1/shell.c
@@ -9,6 +9,8 @@
 #define BUFFER_SIZE 80
 
 typedef void (*fun_ptr) (char* args[]);
+char commands[128][BUFFER_SIZE];
+int size= 0;
 
 void help(char* args[]){
 	printf("The built-in commands are:\nexit\nhelp\ncd\n");
@@ -26,8 +28,22 @@ void endShell(char* args[]){
 
 void cd(char* args[]){
 	int a;
+	if(args[1]==NULL){
+		printf("Error: null\n");
+		return;
+	}
 	a = chdir(args[1]);
 	if(a!=0) printf("Invalid directory\n");
+	return;
+}
+
+void history(char* args[]){
+	printf("History:\n");
+	int j = 0;
+	for(j = 0; j<size; j++){
+		printf("%s\n", commands[j]);
+	}
+	j = 0;
 	return;
 }
 
@@ -54,47 +70,114 @@ void otherCommands(char* args[]){
 	return;
 }
 
+
+
 void handleInputs(char* args[], char* builtins[], fun_ptr functions[]){
-	int i;
+	int index = 0;
 	int found = 0;
-	while(builtins[i]!=NULL){
-		if(strcmp(args[0], builtins[i]) == 0){
-			functions[i](args);
+	while(builtins[index]!=NULL){
+		if(strcmp(args[0], builtins[index]) == 0){
+			functions[index](args);
 			found = 1;
 			break;
 		}
-	i++;
-	if(i>=3) break;	
+	index++;
+	if(index>=4) break;	
 	}
 	if(found==0) otherCommands(args);
+	index = 0;
 	return;
 }
 
-void handleInputs2(char* args1[], char* args2[], char* builtins[], fun_ptr functions[]){
-	int i;
+int checkBuiltins(char* args[], char* builtins[], fun_ptr functions[]){
+        int index = 0;
         int found = 0;
-        while(builtins[i]!=NULL){
-		if(strcmp(args[0], builtins[i]) == 0){
-		        functions[i](args);
-	                found = 1;
-	                break;
-	        }
-		i++;
-		if(i>=3) break;
+        while(builtins[index]!=NULL){
+                if(strcmp(args[0], builtins[index]) == 0){
+                        functions[index](args);
+                        found = 1;
+                        break;
+                }
+	        index++;
+	        if(index>=4) break;
+        }
+        index = 0;
+        return found;
+}
+
+
+void handleTwoInputs(char* args1[], char* args2[], char* builtins[], fun_ptr functions[]){
+	//make the pipe
+	int fd[2];
+	pid_t split1, split2;
+	if (pipe(fd)<0){
+		printf("Pipe error\n");
+		return;
 	}
-        //TODO: search bin, then use pipe	
+	//fork the first command
+	split1 = fork();
+	if(split1<0){
+		printf("Fork error\n");
+		return;
+	}
+	if(split1 == 0){//child of first fork
+		//write to start of pipe
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+
+		int firstx = checkBuiltins(args1, builtins, functions);
+		if(firstx==0){
+			if(execvp(args1[0], args1)<0){
+				printf("Incorrect command -- try again\n");
+				exit(0);
+			}
+		}
+		exit(0);
+		
+	}
+	else{//parent
+		//fork the second command
+		split2 = fork();
+		if(split2<0){
+			printf("Fork error\n");
+			return;
+		}
+		if(split2==0){
+			//read from the pipe output to stdin
+			wait(NULL);
+			close(fd[1]);
+			dup2(fd[0], STDIN_FILENO);
+		        close(fd[0]);
+			int secondx = checkBuiltins(args2, builtins, functions);
+		        if(secondx==0){
+				printf("got here\n");
+				if(execvp(args2[0], args2)<0){
+	                        	printf("Incorrect command -- try again\n");                                     
+					exit(0);
+	                	}
+	        	}
+			exit(0);	
+		}
+		else{
+			//wait for children to wrap up
+			wait(NULL);
+			wait(NULL);
+		}
+	}
+	return;
 }
 
 int main(int argc, char** argv){
 	signal(SIGINT, sigint_handler);
 	char inputs[BUFFER_SIZE];
-	char* builtins[] = {"exit", "help", "cd"};
+	char* builtins[] = {"exit", "help", "cd", "history"};
 	//typedef void (*fun_ptr) (char[]);
-	fun_ptr functions[3];
+	fun_ptr functions[4];
 	functions[0] = &endShell;
 	functions[1] = &help;
 	functions[2] = &cd;
-	functions[3] = NULL;
+	functions[3] = &history;
 	while(1){
 		//start by printing line prompt and reading input
 		printf("mini-shell> ");
@@ -117,9 +200,16 @@ int main(int argc, char** argv){
 		//tokenize arguments
 		tokenize(first, args1);
 		tokenize(second, args2);
+		strcpy(commands[size++], args1[0]);
+		size = size % 128;
 		//process arguments
 		if(foundPipe==0) handleInputs(args1, builtins, functions);
 		//functions in case there is a pipe
+		else{
+		strcpy(commands[size++], args2[0]);
+	          size= size % 128;	
+		 handleTwoInputs(args1, args2, builtins, functions);
+		}
 
 	}	
 	return 0;
